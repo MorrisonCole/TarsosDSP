61f97bf6ffd4f9ca14d525722b61b1c026b40e9e
Steve Rubin <nerdforlife@gmail.com>
2014-03-02 19:29:54 -0800

    update, removing lots of things that aren't going to work without major tweaks

--- src/be/hogent/tarsos/dsp/AudioEvent.java
+++ src/be/hogent/tarsos/dsp/AudioEvent.java
@@ -143,6 +143,13 @@ public class AudioEvent {
 		}
 	}
 	
+	public void setFloatBufferWithByteBuffer(byte[] byteBuffer) {
+		System.out.println("frame size and length: " + format.getFrameSize() + " " + byteBuffer.length);
+		int length = (int)(byteBuffer.length / (float)(format.getFrameSize()));
+		this.floatBuffer = new float[length];
+		converter.toFloatArray(byteBuffer, this.floatBuffer);
+	}
+	
 	public void setFloatBuffer(float[] floatBuffer) {
 		this.floatBuffer = floatBuffer;
 	}
